c457347592bb18c164801903981ccdb7e120e418
kohkimakimoto <kohki.makimoto@gmail.com>
2013-04-30 18:46:28 +0900

    Modify css

--- modules/manager/home.php
+++ modules/manager/home.php
@@ -17,7 +17,7 @@ $hosts = AppHost::getAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="<?php echo AppConfig::get('base_url')?>/statics/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo AppConfig::get('base_url')?>/statics/css/bootstrap.css" rel="stylesheet">
     <link href="<?php echo AppConfig::get('base_url')?>/statics/css/style.css" rel="stylesheet">
     <script src="<?php echo AppConfig::get('base_url')?>/statics/js/jquery-1.8.1.min.js"></script>
     <script src="<?php echo AppConfig::get('base_url')?>/statics/js/bootstrap.js"></script>
