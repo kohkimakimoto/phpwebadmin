bb4c17fde89648cd198b1812872eee00549a707a
kohkimakimoto <kohki.makimoto@gmail.com>
2013-05-01 19:37:41 +0900

    Under the development

--- modules/genghis/index.php
+++ modules/genghis/index.php
@@ -5,5 +5,10 @@ require AppConfig::get('system_dir')."/include/protect.php";
 
 $params = AppConfig::get('hosts/'.$_GET['feature'].'/params');
 
+chdir(__DIR__);
+$_SERVER["SCRIPT_FILENAME"] = $_SERVER['SCRIPT_URL'];
+$_SERVER["PHP_SELF"] = $_SERVER['SCRIPT_URL'];
+$_SERVER["SCRIPT_NAME"] = $_SERVER['SCRIPT_URL'];
+
 require AppConfig::get('system_dir').'/vendor/genghis/genghis.php';
 
