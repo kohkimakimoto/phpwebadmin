5452de9b613e8da7b57573b2d9947281873fdfa2
kohkimakimoto <kohki.makimoto@gmail.com>
2013-05-10 19:05:27 +0900

    Under the development

--- modules/master/_footer.php
+++ modules/master/_footer.php
@@ -7,7 +7,7 @@ if (!defined('WA_ROOT')) {
     <div class="content-footer">
       <div class="container-front">
         <p>
-        Design is based on <a href="http://twitter.github.io/bootstrap/index.html2">twitter bootstrap</a>.
+        Design is based on <a href="http://twitter.github.io/bootstrap/index.html">twitter bootstrap</a>.
         </p>
       </div>
     </div>
